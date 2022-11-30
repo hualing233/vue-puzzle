40fc27d2b3f3c932120abff895ba4d5009087412
hualing233 <wblai@foxmail.com>
2022-11-30 23:05:14 +0800

    feat: vite打包配置调整更新打包文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [vue()],
+      server: {
+        host: '0.0.0.0',
+      },
+    };
+  } else {
+    return {
+      base: '/vue-puzzle/',
+      plugins: [vue()],
+    };
   }
-})
+});
